ca0ea3427b4511af5d3864643d2894188ec18c63
TaeSeop Jeong <64714646+awaxs@users.noreply.github.com>
2023-10-15 08:25:58 +0900

    Add files via upload

---
+++ precache-manifest.dc1bb4a91a8ae4d31e399d700d149e67.js
@@ -0,0 +1,43 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+<<<<<<<< HEAD:build/precache-manifest.a81bd889a93243dc96a60692b1953101.js
+    "revision": "0fda6cc2162c873443fe1b5188ff7bdf",
+    "url": "/hawon/index.html"
+  },
+  {
+    "revision": "ada68cc571bda5bc43f6",
+========
+    "revision": "a9324e88b70417d081c7899c20927205",
+    "url": "/hawon/index.html"
+  },
+  {
+    "revision": "3398badc2d29629234db",
+>>>>>>>> 11ae9162916b18fa4b8f239e8b27990e89e26d9c:build/precache-manifest.dc1bb4a91a8ae4d31e399d700d149e67.js
+    "url": "/hawon/static/css/main.5b1cbd55.chunk.css"
+  },
+  {
+    "revision": "ea4d0eebb0e809917e85",
+    "url": "/hawon/static/js/2.8cb50717.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/hawon/static/js/2.8cb50717.chunk.js.LICENSE.txt"
+  },
+  {
+<<<<<<<< HEAD:build/precache-manifest.a81bd889a93243dc96a60692b1953101.js
+    "revision": "ada68cc571bda5bc43f6",
+    "url": "/hawon/static/js/main.4bd15467.chunk.js"
+========
+    "revision": "3398badc2d29629234db",
+    "url": "/hawon/static/js/main.9ba0200e.chunk.js"
+>>>>>>>> 11ae9162916b18fa4b8f239e8b27990e89e26d9c:build/precache-manifest.dc1bb4a91a8ae4d31e399d700d149e67.js
+  },
+  {
+    "revision": "c9486fdd2f8c0ce6d2a4",
+    "url": "/hawon/static/js/runtime-main.06c7e197.js"
+  },
+  {
+    "revision": "43327df2d26ec39d75440f5caf407074",
+    "url": "/hawon/static/media/AI.43327df2.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,43 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+<<<<<<< HEAD
+  "/hawon/precache-manifest.a81bd889a93243dc96a60692b1953101.js"
+=======
+  "/hawon/precache-manifest.dc1bb4a91a8ae4d31e399d700d149e67.js"
+>>>>>>> 11ae9162916b18fa4b8f239e8b27990e89e26d9c
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hawon/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
