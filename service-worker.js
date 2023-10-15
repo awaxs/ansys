d085defe5b87953f33d0a25c73bdcb90f6e8f067
TaeSeop Jeong <64714646+awaxs@users.noreply.github.com>
2023-10-15 09:50:03 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
-  "/hawon/precache-manifest.a81bd889a93243dc96a60692b1953101.js"
-=======
-  "/hawon/precache-manifest.dc1bb4a91a8ae4d31e399d700d149e67.js"
->>>>>>> 11ae9162916b18fa4b8f239e8b27990e89e26d9c
+  "/hawon/precache-manifest.2c289703dc72d90da4eacb965cf93243.js"
 );
 
 self.addEventListener('message', (event) => {
